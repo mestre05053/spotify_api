b3d05854c6040f87c3a5d13fbdebf4b39b77bea8
mestre05053 <mestre05053@gmail.com>
2024-01-11 18:24:32 -0600

    Encontre el endpoint de las canciones mas escuchadas

--- spotify_api/main.py
+++ spotify_api/main.py
@@ -36,7 +36,10 @@ artist_id = artist['id']
 
 #############
 # Mi codigo
+
+### URLS
 artistas_mas_escuchados = 'me/top/artists'
+canciones_mas_escuchados = 'me/top/tracks'
 
 params = {
     'time_range' : "long_term",
@@ -76,7 +79,11 @@ def top_10():
         print('Encoding:',response.encoding)
         print('-------------------------------------')
 
-        counter = 1
+        '''
+        Los 10 artistas más escuchados por el usuario
+        - A través de esos 10 artistas, obtener una lista de los 5 géneros musicales favoritos de dicho usuario
+        '''
+        counter = 0
         list_genres = []
         print('Los 10 artistas más escuchados por el usuario')
         for i in response.json()["items"]:
@@ -88,9 +95,17 @@ def top_10():
                         list_genres.append(i)
                     counter_list_genres = Counter(list_genres).most_common(5)
                     counter +=1
+        
         print('-------------------------------------')
-        print('Los 5 géneros musicales favoritos de dicho usuario')                              
-        print(counter_list_genres)  
+        print('Los 5 géneros musicales favoritos mas las veces que se encuentro en la lista')                              
+        print(counter_list_genres) 
+
+        '''
+        Las 10 canciones más escuchadas por el usuario y sus respectivos artistas
+        '''
+        response = requests.get(base_url+artistas_mas_escuchados, headers=headers, params=params)
+
+
 top_10()
                     
                     
